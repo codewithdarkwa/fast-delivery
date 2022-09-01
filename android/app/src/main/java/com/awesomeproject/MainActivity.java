695ff5159c4e1314094490bfce3a6a9e4c04b053
codewithdarkwa <jnrdrkw1@gmail.com>
2022-09-01 15:34:27 +0000

    update

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.awesomeproject;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
