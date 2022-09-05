55a25f8d4409e8634b920321809db454273230dd
codewithdarkwa <jnrdrkw1@gmail.com>
2022-09-05 16:11:02 +0000

    update the local foods price

--- localFoods.js
+++ localFoods.js
@@ -1,83 +1,83 @@
 export const localFoods = [
     {
       name: "Gari and Beans",
-      title: "Gari and Beans",
       image_url:
       "https://kuulpeeps.com/wp-content/uploads/2020/07/0777DA02-E5D5-4917-A1AF-D1EDA6AE5301-1024x1024.jpeg",
       categories: ["Cafe", "Bar"],
       price: "13.50",
       description:"Gari and Beans with fried plantain and egg",
       reviews: 1244,
+      title: "Gari and Beans",
       rating: 4.5,
       activeTab: "Delivery"
     },
     {
       name: "Fufu",
-      title: "Fufu",
       image_url:
       "https://miro.medium.com/max/638/1*XDjyQ6dBVMdybEySuQlHGw.jpeg",
       categories: ["Cafe", "Bar"],
-      price: "13.50",
+      price: "15.0",
       description:"Fufu with light soup and goat meat",
       reviews: 1244,
       rating: 4.5,
+      title: "Fufu",
       activeTab: "pickup"
     },
     {
       name: "Jollof",
-      title: "Jollof",
       image_url: "https://media-cdn.tripadvisor.com/media/photo-s/19/2d/e4/a7/this-is-ghana-jollof.jpg",
       categories: ["Cafe", "Bar"],
-      price: "13.50",
+      price: "20.00",
       description:"Jollof with lettuce and salad ",
       reviews: 1244,
+      title: "Jollof",
       rating: 3.7,
       activeTab: "Delivery"
     },
     {
-      name: "Waakye",
       title: "Waakye",
       image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOh4WahNUlIQxYKTZe3sMigSXHZPhH-awNJw&usqp=CAU",
       categories: ["Cafe", "Bar"],
-      price: "13.50",
+      price: "10.50",
       description:"Waakye with salad and meat",
       reviews: 1244,
+      name: "Waakye",
       rating: 4.7,
       activeTab: "Pickup"
     },
     {
       name: "Banku",
-      title: "Banku",
       image_url:"https://www.streetfoodguy.com/wp-content/uploads/2019/07/banku-tilpaia-1-1.jpg",
       
       categories: ["Indian", "Bar"],
-      price: "13.50",
+      price: "14.00",
       description:"Banku with peper fried fish ",
       reviews: 700,
+      title: "Banku",
       rating: 4.9,
       activeTab: "Delivery"
     },
     {
       name: "Fried rice",
-      title: "Fried rice",
       image_url:
       "https://images.bolt.eu/store/2021/2021-12-02/7c27d841-ff96-4a2a-8a43-1a63e2b112b4.jpeg",
       categories: ["Indian", "Bar"],
-      price: "13.50",
+      price: "25.50",
       description:"Fried rice with chicken",
       reviews: 700,
+      title: "Fried rice",
       rating: 4.9,
       activeTab: "Pickup"
     },
     {
       name: "Rice ball with Groundnut soup",
-      title: "Rice ball with Groundnut soup",
       image_url:
       "https://i.ytimg.com/vi/xAtWPgIINTY/maxresdefault.jpg",
       categories: ["Cafe", "Bar"],
-      price: "13.50",
+      price: "12.50",
       description:"Rice ball with Groundnut soup and goat meat",
       reviews: 1244,
+      title: "Rice ball with Groundnut soup",
       rating: 4.0,
       activeTab: "Delivery"
     },
@@ -87,7 +87,7 @@ export const localFoods = [
       image_url:
       "https://cdn.shopify.com/s/files/1/1153/4418/articles/kenkey_with_fish_1600x.PNG?v=1527109158",
       categories: ["Cafe", "Bar"],
-      price: "13.50",
+      price: "9.00",
       description:"Kenkey with fried fish and hot peper",
       reviews: 1244,
       rating: 4.5,
@@ -95,49 +95,49 @@ export const localFoods = [
     },
     {
       name: "Ampesi",
-      title: "Ampesi",
       image_url:
       "https://wordpress.ghanatalksradio.com/wp-content/uploads/2022/03/IMG_20181010_210531_331.jpg",
       categories: ["Cafe", "Bar"],
-      price: "13.50",
+      price: "10.00",
       description:"Ampesi with kontomire stew",
       reviews: 1244,
+      title: "Ampesi",
       rating: 4.5,
       activeTab: "Pickup"
     },
     {
       name: "Ampesi",
-      title: "Ampesi",
       image_url:
       "https://agameals.com/wp-content/uploads/2022/02/6064e78d05eb348a7c6e18ad6a63abb8-260x195.jpg",
       categories: ["Cafe", "Bar"],
-      price: "13.50",
+      price: "8.00",
       description:"Ampesi with kontomire stew",
       reviews: 1244,
+      title: "Ampesi",
       rating: 4.5,
       activeTab: "Delivery"
     },
     {
       name: "Fufu",
-      title: "Fufu",
       image_url:
       "https://i.pinimg.com/originals/d1/91/88/d19188f6dcc876c7181db2797e951b70.jpg",
       categories: ["Cafe", "Bar"],
-      price: "13.50",
+      price: "30.00",
       description:"Fufu with light soup and goat meat",
       reviews: 1244,
+      title: "Fufu",
       rating: 4.5,
       activeTab: "Delivery"
     },
     {
       name: "Fried plantain ",
-      title: "Fried plantain ",
       image_url:
       "https://eatyourworld.com/blog/wp-content/uploads/2021/06/kelewele-ghana-foods.jpg",
       categories: ["Cafe", "Bar"],
-      price: "13.50",
+      price: "10.00",
       description:"fried plantain",
       reviews: 1244,
+      title: "Fried plantain ",
       rating: 4.5,
       activeTab: "Pickup"
     },
