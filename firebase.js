c394342e4d658e5e0dbec9a7e012fb5b6c62ffd7
codewithdarkwa <jnrdrkw1@gmail.com>
2022-09-07 13:26:50 +0000

    update

--- firebase.js
+++ firebase.js
@@ -14,18 +14,3 @@ const firebaseConfig = {
 
 
   export default firebase;
-
-// import { initializeApp } from "firebase/app"
-// import { getFirestore } from 'firebase/firestore'
-
-//  const firebaseConfig = {
-//   apiKey: "AIzaSyCEtFbxO0_9NaRKYljESsCcFlX_h0lyyjk",
-//   authDomain: "fast-delivery-26899.firebaseapp.com",
-//   projectId: "fast-delivery-26899",
-//   storageBucket: "fast-delivery-26899.appspot.com",
-//   messagingSenderId: "174389002001",
-//   appId: "1:174389002001:web:fca8fe7b66ae8bb2ca56b0"
-//  };
-
-//  const app = initializeApp(firebaseConfig);
-//  export const db = getFirestore(app);
